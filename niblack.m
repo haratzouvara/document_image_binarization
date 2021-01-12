729cc9b46948199a9304b6fa969e202da7d48d39
haratzouvara <49521954+haratzouvara@users.noreply.github.com>
2021-01-12 23:53:58 +0200

    Update niblack.m

--- niblack.m
+++ niblack.m
@@ -1,5 +1,5 @@
 %read image -- put image path
-image = imread('143.png');
+image = imread('image.jpg');
 
 % convert image to grayscale
 gray = rgb2gray(image);
@@ -7,22 +7,24 @@ gray = rgb2gray(image);
 % window
 w=25;
 
-% convert to double
-gray = double(gray);
-
-% Mean value   %% averagefilter is provided by matlab  
-mean = averagefilter(gray,[w w],'replicate');
-
-%Standar deviation
-meanSquare = averagefilter(gray.^2,[w w], 'replicate');  
-deviation    = (meanSquare-mean.^2).^0.5;
-
 %k value
 k_value = -0.5;
 
+% convert to double
+gray = double(gray);
+
+
+% mean value   
+filterWindow = ones(w,w) /(w*w);
+mean = imfilter(gray, filterWindow, 'replicate' );
+    
+%Standar deviation
+meanSquare = imfilter(gray.^2,filterWindow, 'replicate' ) ;  
+deviation = (meanSquare-mean.^2).^0.5;
+
+
 binary = zeros(size(gray));
-binary(gray > mean + k *deviation) = 1;
+binary(gray > mean + k_value *deviation) = 1;
 
 figure, imshow(binary);
-
 
